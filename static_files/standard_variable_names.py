74b1c90c73a6e0b6aaf997f1339a2633c9b4257d
boygioykaskyriakos <boygioykaskyriakos@gmail.com>
2020-07-06 22:24:51 +0300

    added more constants

--- static_files/standard_variable_names.py
+++ static_files/standard_variable_names.py
@@ -18,3 +18,12 @@ INDEX_FIRST_ELEMENT = "index_first_element"
 INDEX_LAST_ELEMENT = "index_last_element"
 CONFIDENCE_LVL = "confidence_lvl"
 TOTAL_PANICS = "total_panics"
+
+LOWER_RANGE_NUMBER = "lower_range_number"
+HIGHER_RANGE_NUMBER = "higher_range_number"
+
+UPPER_NUMERATOR = "upper_numerator"
+LOWER_NUMERATOR = "lower_numerator"
+LOWER_DENOMINATOR = "lower_denominator"
+UPPER_DENOMINATOR = "upper_denominator"
+EXCEPTION = "exception"
